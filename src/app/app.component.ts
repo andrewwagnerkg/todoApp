dfc98fefef7a972d843be8112f8de0ae3df0bd8e
Andrei Wagner <andrei.vagner@av.av>
2022-08-27 15:02:44 +0600

    add input elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataModel, TodoItem } from './model';
 
 @Component({
@@ -6,15 +6,26 @@ import { DataModel, TodoItem } from './model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'todoApp';
+  addTodoActionText = ""
   model = new DataModel();
+
+  public ngOnInit(): void {
+
+  }
 
   public GetName(){
     return this.model.Name;
   }
 
   public GetItems(){
-    return this.model.Items;
+    return this.model.Items.filter(item => !item.Done);
+  }
+
+  public AddTodo():void{
+    this.model.Items.push(new TodoItem(this.addTodoActionText, false))
+    this.addTodoActionText = ''
   }
 }
